2507ce3ee9fdfd228082cb60104cbd6a96819ac1
unknown <Mridang@Mridang-PC.(none)>
2013-05-31 12:17:53 +0300

    Modified to install the sqlite binary if missing

--- src/com/mridang/hangouts/HangoutsWidget.java
+++ src/com/mridang/hangouts/HangoutsWidget.java
@@ -1,11 +1,15 @@
 package com.mridang.hangouts;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.TimeoutException;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -34,9 +38,15 @@ public class HangoutsWidget extends DashClockExtension {
 		Log.d("HangoutsWidget", "Created");
 		BugSenseHandler.initAndStartSession(this, "3279a667");
 
-		PackageManager pkgManager = getPackageManager();
-		ittApplication = pkgManager.getLaunchIntentForPackage("com.google.android.talk");
-		ittApplication.addCategory(Intent.CATEGORY_LAUNCHER);
+		try {
+
+			PackageManager pkgManager = getPackageManager();
+			ittApplication = pkgManager.getLaunchIntentForPackage("com.google.android.talk");
+			ittApplication.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		} catch (Exception e) {
+			return;
+		}
 
 	}
 
@@ -66,7 +76,7 @@ public class HangoutsWidget extends DashClockExtension {
 					Log.d("HangoutsWidget", "Reading unread messages from the databases");
 					try {
 
-						Command command = new Command(0, "cd  /data/data/com.google.android.talk/databases/", "sqlite3 babel1.db \"SELECT author_full_name FROM message_notifications_view WHERE notified_for_failure = 0 AND type = 2;\"") {
+						Command cmdQuery = new Command(0, "cd  /data/data/com.google.android.talk/databases/", "sqlite3 babel1.db \"SELECT author_full_name FROM message_notifications_view WHERE notified_for_failure = 0 AND type = 2;\"") {
 
 							@Override
 							public void output(int id, String strLine) {
@@ -86,12 +96,13 @@ public class HangoutsWidget extends DashClockExtension {
 							}
 
 						};
-						RootTools.getShell(true).add(command).waitForFinish();
+						RootTools.getShell(true).add(cmdQuery).waitForFinish();
 
 						Integer intMessages = Integer.parseInt(edtInformation.status() == null ? "0" : edtInformation.status());
 						edtInformation.status(getResources().getQuantityString(R.plurals.message, intMessages, intMessages));
 						edtInformation.visible(intMessages > 0);
-						edtInformation.clickIntent(ittApplication);
+						if (ittApplication != null)
+							edtInformation.clickIntent(ittApplication);
 						Log.d("HangoutsWidget", intMessages + " unread");
 
 					} catch (InterruptedException e) {
@@ -108,8 +119,60 @@ public class HangoutsWidget extends DashClockExtension {
 					}
 
 				} else {
+
 					Log.w("HangoutsWidget", "Sqlite executable doesn't seem to be installed");
-					BugSenseHandler.sendException(new Exception("Sqlite executable doesn't seem to be installed"));
+					AssetManager assManager = this.getAssets();
+
+					Log.d("HangoutsWidget", "Unpacking the Sqlite binary");
+					InputStream istData = null;
+					OutputStream ostFile = null;
+					try {
+
+						istData = assManager.open("sqlite3");
+						String strFilename = "/data/data/" + this.getPackageName() + "/sqlite3";
+						ostFile = new FileOutputStream(strFilename);
+
+						byte[] buffer = new byte[1024];
+						int read;
+						while ((read = istData.read(buffer)) != -1) {
+							ostFile.write(buffer, 0, read);
+						}
+						istData.close();
+						istData = null;
+						ostFile.flush();
+						ostFile.close();
+						ostFile = null;
+
+						Log.d("HangoutsWidget", "Installled successfully");
+
+					} catch (Exception e) {
+						Log.e("HangoutsWidget", "Error unpacking Sqlite", e);
+						BugSenseHandler.sendException(e);
+						return;
+					}
+
+					Log.d("HangoutsWidget", "Installing the Sqlite binary");
+					try {
+
+						Command cmdInstall = new Command(0, "mount -o remount,rw /system", "cp /data/data/" + this.getPackageName() + "/sqlite3 /system/bin/sqlite3", "chmod 4755 /system/bin/sqlite3", "mount -o remount,ro /system") {
+
+							@Override
+							public void output(int arg0, String strLine) {								
+
+								System.out.println(strLine);								
+
+							}
+
+						};
+						RootTools.getShell(true).add(cmdInstall).waitForFinish();
+						Log.d("HangoutsWidget", "Installled successfully");
+
+					} catch (Exception e) {
+						Log.e("HangoutsWidget", "Error installing Sqlite", e);
+						BugSenseHandler.sendException(e);
+						return;
+					}
+
 				}	
 
 			} else {
